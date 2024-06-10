18e4b423374d07149b41dec172b1e84d83eed579
VladyslavHn <vlad.gnatok@gmail.com>
2024-06-10 20:31:54 +0200

    123

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ import pino from 'pino';
 import expressPino from 'express-pino-logger';
 import { env } from "./utils/env.js";
 import express from 'express';
-import { getContacts, getContactById } from "./services/services.js";
-import mongoose from 'mongoose';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import contactsRouter from './routers/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -12,53 +13,19 @@ export const setupServer = () => {
     const app = express();
     app.use(cors());
 
-    // Створення логера
+
     const logger = pino({
         transport: {
             target: 'pino-pretty',
         }
     });
 
-    // Використання middleware express-pino-logger
     app.use(expressPino({ logger }));
 
-    app.get('/contacts', async (req, res) => {
-        const contacts = await getContacts();
-        res.status(200).json({
-            status: 'OK',
-            message: 'Contacts found',
-            data: contacts
-        })
-    });
+    app.use(contactsRouter)
 
-    app.get('/contacts/:id', async (req, res) => {
-        const { id } = req.params;
-
-        if (!mongoose.Types.ObjectId.isValid(id)) { 
-            return res.status(400).json({
-                message: 'Not found'
-            });
-        }
-        const contact = await getContactById(id);
-        
-        if (!contact) {
-            return res.status(404).json({
-                message: 'Not found contact with this id'
-            });
-        }
-        
-        res.status(200).json({
-            status: 'OK',
-            message: 'Contact found',
-            data: contact
-        });
-    });
-
-    app.use('*', (req, res) => {
-        res.status(404).json({
-            message: 'Not found'
-        });
-    });
+    app.use('*', notFoundHandler);
+    app.use(errorHandler);
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
--- src/services/services.js
+++ src/services/services.js
@@ -9,3 +9,32 @@ export const getContactById = async (id) => {
     const data = await contacts.findById(id);
     return data;
 }
+
+export const createContact = async (payload) => {
+    const data = await contacts.create(payload);
+    return data;
+}
+
+export const upsertContact = async (id, payload) => {
+    const rawResult = await contacts.findOneAndUpdate(
+        { _id: id },
+        payload,
+        {
+            new: true,
+            includeResultMetaData: true,
+            ...options
+        },
+    );
+
+    if (!rawResult || rawResult.value) return null;
+
+    return {
+        contact: rawResult.value,
+        isNew: Boolean(rawResult?.lastErrorObject?.upsert)
+    }
+}
+
+export const deleteContact = async (id) => { 
+    const data = await contacts.findOneAndDelete({ _id: id });
+    return data;
+}
