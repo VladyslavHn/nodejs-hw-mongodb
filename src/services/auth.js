73d6d00608f906118128e98d8d29896aefa3119c
VladyslavHn <vlad.gnatok@gmail.com>
2024-07-02 23:59:08 +0200

    123

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,6 +6,12 @@ import createHttpError from 'http-errors';
 import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
 import { SessionsCollection } from '../db/models/Session.js';
 
+import jwt from 'jsonwebtoken';
+
+import { SMTP } from '../constants/index.js';
+import { env } from '../utils/env.js';
+import { sendMail } from '../utils/sendMail.js';
+
 export const registerUser = async (payload) => {
   const user = await UsersCollection.findOne({ email: payload.email });
   if (user) throw createHttpError(409, 'Email in use');
@@ -91,5 +97,22 @@ export const requestResetToken = async (email) => {
   if (!user) {
     throw createHttpError(404, 'User not found');
   }
-  
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    env('JWT_SECRET'),
+    {
+      expiresIn: '5m',
+    }
+  );
+
+  await sendMail({
+    from: env(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+  })
 }
+
