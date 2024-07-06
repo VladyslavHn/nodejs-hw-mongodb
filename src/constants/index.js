b0f585373dc2a9808f57643bff41f03b6a7f0bac
VladyslavHn <vlad.gnatok@gmail.com>
2024-07-06 22:10:13 +0200

    123

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,10 +17,10 @@ export const SMTP = {
   JWT_SECRET: 'JWT_SECRET',
 };
 
-
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const CLOUDINARY = {
@@ -29,3 +29,5 @@ export const CLOUDINARY = {
   API_SECRET: 'API_SECRET',
   ENABLE_CLOUDINARY: 'ENABLE_CLOUDINARY',
 };
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -22,7 +23,9 @@ export const setupServer = () => {
     }),
   );
 
-    app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  
+     app.use('/api-docs', swaggerDocs());
 
   app.use(cors());
 
@@ -35,6 +38,8 @@ export const setupServer = () => {
   );
 
   app.use(router);
+
+
 
   app.use('*', notFoundHandler);
 
